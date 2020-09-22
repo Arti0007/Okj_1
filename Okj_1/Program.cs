9527c8b15e210a600c33c4b1a9b0652a00b07cc1
Máté Vincze <vinczemate9@gmail.com>
2020-09-22 10:18:56 +0200

    asd

---
+++ Okj_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Okj_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Vincze Máté");
+            Console.ReadKey(true);
+        }
+    }
+}
