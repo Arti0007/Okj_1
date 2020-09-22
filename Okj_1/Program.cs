8bee530fb08a7425082294072a626ca2b42d42b1
Máté Vincze <vinczemate9@gmail.com>
2020-09-22 10:27:12 +0200

    Add 100 csillag

--- Okj_1/Program.cs
+++ Okj_1/Program.cs
@@ -4,10 +4,19 @@ namespace Okj_1
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Vincze Máté");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+                Console.SetCursorPosition(
+                    rnd.Next(Console.WindowWidth),
+                    rnd.Next(Console.WindowHeight));
+                    Console.Write("*");
+            }
             Console.ReadKey(true);
         }
     }
